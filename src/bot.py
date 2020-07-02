3bdab5f19eccfe69463bc8b2e270fedd6dbe43e8
Bobbbay <49123594+Bobbbay@users.noreply.github.com>
2020-07-02 10:19:21 -0400

    Create bot.py

---
+++ src/bot.py
@@ -0,0 +1,48 @@
+import praw
+import os
+import re
+import json
+
+sub = "BossBye"
+
+client_id = os.environ.get('client_id')
+client_secret = os.environ.get('client_secret')
+password = os.environ.get('pass')
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     password=password,
+                     user_agent='r/BossBye bot',
+                     username='SnooPaintings9')
+
+print("It worked!")
+
+
+moderators = list(reddit.subreddit(sub).moderator())
+for submission in reddit.subreddit(sub).new(limit=None):
+    reply = "Mod replies: \n\n "
+    editing = ""
+    print("Started")
+    print(submission.title)
+    if(submission.link_flair_text == "News"):
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            print("Going through comments")
+            if (comment.author in moderators and comment.author != "SnooPaintings9"):
+                reply +="[Comment by u/{0}](https://reddit.com/r/BossBye/comments/{2}/{3}/{4}/): \n\n>{1} \n\n".format(comment.author, comment.body, submission.id, submission.title, comment.id)
+            elif (comment.author == "SnooPaintings9"):
+                editing = comment.id
+    
+        if editing is not "":
+            comment = reddit.comment(editing)
+            reply += "^Beep ^boop. This is a bot providing a service. If you have any questions, please [contact the moderators](https://www.reddit.com/message/compose?to=/r/BossBye)."
+            if(reply == "Mod replies: \n\n ^Beep ^boop. This is a bot providing a service. If you have any questions, please [contact the moderators](https://www.reddit.com/message/compose?to=/r/BossBye)."):
+                comment.edit("No mod comments yet. ")
+            else:
+                comment.edit(reply)
+        else:
+            reply += "^Beep ^boop. This is a bot providing a service. If you have any questions, please [contact the moderators](https://www.reddit.com/message/compose?to=/r/BossBye)."
+            if(reply == "Mod replies: \n\n ^Beep ^boop. This is a bot providing a service. If you have any questions, please [contact the moderators](https://www.reddit.com/message/compose?to=/r/BossBye)."):
+                submission.reply("No mod comments yet. ").mod.distinguish(sticky=True)
+            else:
+                submission.reply(reply).mod.distinguish(sticky=True)
